bf7407813c63fdecb7677d2e8faaac90e9fa8560
rbdakee <104767440+rbdakee@users.noreply.github.com>
2023-09-24 03:32:15 +0600

    debug=False

--- usharal.kz/main.py
+++ usharal.kz/main.py
@@ -627,5 +627,5 @@ def error_page(error):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000, host='0.0.0.0')
+    app.run(debug=False, port=8000, host='0.0.0.0')
     # ssl_context='adhoc'
