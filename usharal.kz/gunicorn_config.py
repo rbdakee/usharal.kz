8eb1ebd5de550c5374afd37ed6dfe9a3379ccdc5
server <usharal.market@gmail.com>
2023-09-25 13:07:58 +0000

    uploaded version

---
+++ usharal.kz/gunicorn_config.py
@@ -0,0 +1,6 @@
+bind = 'unix:/var/run/main.sock'
+workers = 4
+timeout = 60
+certfile = '/home/ubuntu/usharal.kz/usharal.kz/certificate.crt'
+keyfile = '/home/ubuntu/usharal.kz/usharal.kz/private_key.key'
+ca_certs = '/home/ubuntu/usharal.kz/usharal.kz/root_ca.crt'
