1775e0c7e20d3c1049fd4196f4478ffd6c48ff73
rbdakee <104767440+rbdakee@users.noreply.github.com>
2023-09-27 05:07:47 +0600

    main host is ip

--- usharal.kz/main.py
+++ usharal.kz/main.py
@@ -629,4 +629,4 @@ def attributeError_habdler(error):
 
 
 if __name__ == '__main__':
-    socketio.run(app, host='185.22.67.13', port=8000)
+    socketio.run(app, host='185.22.67.13', port=80, certfile='certificate.crt', keyfile='private_key.key')
